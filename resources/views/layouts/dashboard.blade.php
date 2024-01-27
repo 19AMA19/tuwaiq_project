e755495808369e2812e21b4aff453a7f56a763c9
19ama19 <abdulkarim.alansari@outlook.com>
2024-01-27 19:28:54 +0300

    last comment

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -63,42 +63,24 @@
                                         
                                         <li class="nav-item">
                                             <a href="{{route('GetAllItem')}}" class="nav-link align-middle px-0">
-                                                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">All Prodcts</span>
+                                              <span class="ms-1 d-none d-sm-inline">جميع المنتجات</span>
                                             </a>
                                         </li>
 
                                         <li class="nav-item">
                                             <a href="{{route('AddGroupDashboard')}}" class="nav-link align-middle px-0">
-                                                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Add Groups</span>
+                                             <span class="ms-1 d-none d-sm-inline">إضافة مجموعة جديدة</span>
                                             </a>
                                         </li>
                                         
                                         <li>
                                             <a href="{{route('AddNewItemDashboard')}}" class="nav-link px-0 align-middle">
-                                            <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Add Items</span> </a>
+                                            <span class="ms-1 d-none d-sm-inline">إضافة عنصر جديد</span> </a>
                                         </li>
 
-                                        <li>
-                                            <a href="#" class="nav-link px-0 align-middle">
-                                            <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Invoices</span> </a>
-                                        </li>
 
                                     </ul>    
-                                     <div class="dropdown pb-4">
-                                        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                                            <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
-                                            <span class="d-none d-sm-inline mx-1">loser</span>
-                                        </a>
-                                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                                            <li><a class="dropdown-item" href="#">New project...</a></li>
-                                            <li><a class="dropdown-item" href="#">Settings</a></li>
-                                            <li><a class="dropdown-item" href="#">Profile</a></li>
-                                            <li>
-                                                <hr class="dropdown-divider">
-                                            </li>
-                                            <li><a class="dropdown-item" href="#">Sign out</a></li>
-                                        </ul>
-                                    </div>
+                   
                                 </div>
                             </div>
                             <div class="col py-3">
